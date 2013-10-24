d296d2d931fb6a28ffd92428608466217920a715
Zach Ahn <zach.ahn@gmail.com>
2013-10-24 02:40:54 -0400

    fix indentation

--- jni/application/Crate_State.cpp
+++ jni/application/Crate_State.cpp
@@ -7,136 +7,136 @@ using namespace Zeni::Collision;
 
 namespace Crate {
 
-  Crate_State::Crate_State()
-    : m_crate(Point3f(12.0f, 12.0f, 0.0f),
-              Vector3f(30.0f, 30.0f, 30.0f)),
-    m_player(Camera(Point3f(0.0f, 0.0f, 50.0f),
-             Quaternion(),
-             1.0f, 10000.0f),
-             Vector3f(0.0f, 0.0f, -39.0f),
-             11.0f)
-  {
-    set_pausable(true);
-  }
+	Crate_State::Crate_State()
+		: m_crate(Point3f(12.0f, 12.0f, 0.0f),
+							Vector3f(30.0f, 30.0f, 30.0f)),
+		m_player(Camera(Point3f(0.0f, 0.0f, 50.0f),
+						 Quaternion(),
+						 1.0f, 10000.0f),
+						 Vector3f(0.0f, 0.0f, -39.0f),
+						 11.0f)
+	{
+		set_pausable(true);
+	}
 
-  void Crate_State::on_push() {
-    get_Window().set_mouse_state(Window::MOUSE_RELATIVE);
-  }
+	void Crate_State::on_push() {
+		get_Window().set_mouse_state(Window::MOUSE_RELATIVE);
+	}
 
-  void Crate_State::on_key(const SDL_KeyboardEvent &event) {
-    switch(event.keysym.sym) {
-      case SDLK_w:
-        m_controls.forward = event.type == SDL_KEYDOWN;
-        break;
+	void Crate_State::on_key(const SDL_KeyboardEvent &event) {
+		switch(event.keysym.sym) {
+			case SDLK_w:
+				m_controls.forward = event.type == SDL_KEYDOWN;
+				break;
 
-      case SDLK_a:
-        m_controls.left = event.type == SDL_KEYDOWN;
-        break;
+			case SDLK_a:
+				m_controls.left = event.type == SDL_KEYDOWN;
+				break;
 
-      case SDLK_s:
-        m_controls.back = event.type == SDL_KEYDOWN;
-        break;
+			case SDLK_s:
+				m_controls.back = event.type == SDL_KEYDOWN;
+				break;
 
-      case SDLK_d:
-        m_controls.right = event.type == SDL_KEYDOWN;
-        break;
+			case SDLK_d:
+				m_controls.right = event.type == SDL_KEYDOWN;
+				break;
 
-      case SDLK_SPACE:
-        if(event.type == SDL_KEYDOWN) {
-          m_player.jump();
-          m_moved = true;
-        }
-        break;
+			case SDLK_SPACE:
+				if(event.type == SDL_KEYDOWN) {
+					m_player.jump();
+					m_moved = true;
+				}
+				break;
 
-      default:
-        Gamestate_Base::on_key(event);
-        break;
-    }
-  }
+			default:
+				Gamestate_Base::on_key(event);
+				break;
+		}
+	}
 
-  void Crate_State::on_mouse_motion(const SDL_MouseMotionEvent &event) {
-    m_player.adjust_pitch(event.yrel / 500.0f);
-    m_player.turn_left_xy(-event.xrel / 500.0f);
-  }
+	void Crate_State::on_mouse_motion(const SDL_MouseMotionEvent &event) {
+		m_player.adjust_pitch(event.yrel / 500.0f);
+		m_player.turn_left_xy(-event.xrel / 500.0f);
+	}
 
-  void Crate_State::perform_logic() {
-    const Time_HQ current_time = get_Timer_HQ().get_time();
-    float processing_time = float(current_time.get_seconds_since(time_passed));
-    time_passed = current_time;
+	void Crate_State::perform_logic() {
+		const Time_HQ current_time = get_Timer_HQ().get_time();
+		float processing_time = float(current_time.get_seconds_since(time_passed));
+		time_passed = current_time;
 
-    /** Get forward and left vectors in the XY-plane **/
-    const Vector3f forward = m_player.get_camera().get_forward().get_ij().normalized();
-    const Vector3f left = m_player.get_camera().get_left().get_ij().normalized();
+		/** Get forward and left vectors in the XY-plane **/
+		const Vector3f forward = m_player.get_camera().get_forward().get_ij().normalized();
+		const Vector3f left = m_player.get_camera().get_left().get_ij().normalized();
 
-    /** Get velocity vector split into a number of axes **/
-    const Vector3f velocity = (m_controls.forward - m_controls.back) * 50.0f * forward
-                            + (m_controls.left - m_controls.right) * 50.0f * left;
-    const Vector3f x_vel = velocity.get_i();
-    const Vector3f y_vel = velocity.get_j();
-    Vector3f z_vel = m_player.get_velocity().get_k();
+		/** Get velocity vector split into a number of axes **/
+		const Vector3f velocity = (m_controls.forward - m_controls.back) * 50.0f * forward
+														+ (m_controls.left - m_controls.right) * 50.0f * left;
+		const Vector3f x_vel = velocity.get_i();
+		const Vector3f y_vel = velocity.get_j();
+		Vector3f z_vel = m_player.get_velocity().get_k();
 
-    /** Bookkeeping for sound effects **/
-    if(velocity.magnitude() != 0.0f)
-      m_moved = true;
+		/** Bookkeeping for sound effects **/
+		if(velocity.magnitude() != 0.0f)
+			m_moved = true;
 
-    /** Keep delays under control (if the program hangs for some time, we don't want to lose responsiveness) **/
-    if(processing_time > 0.1f)
-      processing_time = 0.1f;
+		/** Keep delays under control (if the program hangs for some time, we don't want to lose responsiveness) **/
+		if(processing_time > 0.1f)
+			processing_time = 0.1f;
 
-    /** Physics processing loop**/
-    for(float time_step = 0.05f;
-        processing_time > 0.0f;
-        processing_time -= time_step)
-    {
-      if(time_step > processing_time)
-        time_step = processing_time;
+		/** Physics processing loop**/
+		for(float time_step = 0.05f;
+				processing_time > 0.0f;
+				processing_time -= time_step)
+		{
+			if(time_step > processing_time)
+				time_step = processing_time;
 
-      /** Gravity has its effect **/
-      z_vel -= Vector3f(0.0f, 0.0f, 50.0f * time_step);
+			/** Gravity has its effect **/
+			z_vel -= Vector3f(0.0f, 0.0f, 50.0f * time_step);
 
-      /** Try to move on each axis **/
-      partial_step(time_step, x_vel);
-      partial_step(time_step, y_vel);
-      partial_step(time_step, z_vel);
+			/** Try to move on each axis **/
+			partial_step(time_step, x_vel);
+			partial_step(time_step, y_vel);
+			partial_step(time_step, z_vel);
 
-      /** Keep player above ground; Bookkeeping for jumping controls **/
-      const Point3f &position = m_player.get_camera().position;
-      if(position.z < 50.0f) {
-        m_player.set_position(Point3f(position.x, position.y, 50.0f));
-        m_player.set_on_ground(true);
-      }
-    }
-  }
+			/** Keep player above ground; Bookkeeping for jumping controls **/
+			const Point3f &position = m_player.get_camera().position;
+			if(position.z < 50.0f) {
+				m_player.set_position(Point3f(position.x, position.y, 50.0f));
+				m_player.set_on_ground(true);
+			}
+		}
+	}
 
-  void Crate_State::render() {
-    Video &vr = get_Video();
+	void Crate_State::render() {
+		Video &vr = get_Video();
 
-    vr.set_3d(m_player.get_camera());
+		vr.set_3d(m_player.get_camera());
 
-    m_crate.render();
-  }
+		m_crate.render();
+	}
 
-  void Crate_State::partial_step(const float &time_step, const Vector3f &velocity) {
-    m_player.set_velocity(velocity);
-    const Point3f backup_position = m_player.get_camera().position;
+	void Crate_State::partial_step(const float &time_step, const Vector3f &velocity) {
+		m_player.set_velocity(velocity);
+		const Point3f backup_position = m_player.get_camera().position;
 
-    m_player.step(time_step);
+		m_player.step(time_step);
 
-    /** If collision with the crate has occurred, roll things back **/
-    if(m_crate.get_body().intersects(m_player.get_body())) {
-      if(m_moved)
-      {
-        /** Play a sound if possible **/
-        m_crate.collide();
-        m_moved = false;
-      }
+		/** If collision with the crate has occurred, roll things back **/
+		if(m_crate.get_body().intersects(m_player.get_body())) {
+			if(m_moved)
+			{
+				/** Play a sound if possible **/
+				m_crate.collide();
+				m_moved = false;
+			}
 
-      m_player.set_position(backup_position);
+			m_player.set_position(backup_position);
 
-      /** Bookkeeping for jumping controls **/
-      if(velocity.k < 0.0f)
-        m_player.set_on_ground(true);
-    }
-  }
+			/** Bookkeeping for jumping controls **/
+			if(velocity.k < 0.0f)
+				m_player.set_on_ground(true);
+		}
+	}
 
 }
--- jni/application/bootstrap.cpp
+++ jni/application/bootstrap.cpp
@@ -17,53 +17,53 @@ using namespace std;
 using namespace Zeni;
 
 class Instructions_State : public Gamestate_Base {
-  Text_Box tb;
+	Text_Box tb;
 
 public:
-  Instructions_State()
-    : tb(Point2f(), Point2f(800.0f, 600.0f), "system_36_800x600",
-    "This demonstrates a simple way of doing collision detection and response in 3D. "
-    "Unfortunately, it is inadequate for more sophisticated scenarios.", Color())
-  {
-    tb.give_BG_Renderer(new Widget_Renderer_Color(get_Colors()["black"]));
-  }
+	Instructions_State()
+		: tb(Point2f(), Point2f(800.0f, 600.0f), "system_36_800x600",
+		"This demonstrates a simple way of doing collision detection and response in 3D. "
+		"Unfortunately, it is inadequate for more sophisticated scenarios.", Color())
+	{
+		tb.give_BG_Renderer(new Widget_Renderer_Color(get_Colors()["black"]));
+	}
 
 private:
-  void on_key(const SDL_KeyboardEvent &event) {
-    if(event.keysym.sym == SDLK_ESCAPE) {
-      if(event.state == SDL_PRESSED)
-        get_Game().pop_state();
-    }
-  }
+	void on_key(const SDL_KeyboardEvent &event) {
+		if(event.keysym.sym == SDLK_ESCAPE) {
+			if(event.state == SDL_PRESSED)
+				get_Game().pop_state();
+		}
+	}
 
-  void render() {
-    get_Video().set_2d(make_pair(Point2f(), Point2f(800.0f, 600.0f)), true);
+	void render() {
+		get_Video().set_2d(make_pair(Point2f(), Point2f(800.0f, 600.0f)), true);
 
-    tb.render();
-  }
+		tb.render();
+	}
 };
 
 class Bootstrap {
-  class Gamestate_One_Initializer : public Gamestate_Zero_Initializer {
-    virtual Gamestate_Base * operator()() {
-      Window::set_title("zenilib Crate Example");
+	class Gamestate_One_Initializer : public Gamestate_Zero_Initializer {
+		virtual Gamestate_Base * operator()() {
+			Window::set_title("zenilib Crate Example");
 
-      get_Controllers();
-      get_Video();
-      get_Textures();
-      get_Fonts();
-      get_Sounds();
+			get_Controllers();
+			get_Video();
+			get_Textures();
+			get_Fonts();
+			get_Sounds();
 
-      return new Title_State<Crate::Crate_State, Instructions_State>("zenilib Example:\nCrate");
-    }
-  } m_goi;
+			return new Title_State<Crate::Crate_State, Instructions_State>("zenilib Example:\nCrate");
+		}
+	} m_goi;
 
 public:
-  Bootstrap() {
-    g_gzi = &m_goi;
-  }
+	Bootstrap() {
+		g_gzi = &m_goi;
+	}
 } g_bootstrap;
 
 int main(int argc, char **argv) {
-  return zenilib_main(argc, argv);
+	return zenilib_main(argc, argv);
 }
